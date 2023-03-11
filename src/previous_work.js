3b601a6dd541d87a8fb85cb4c6dd2bd66b57ab90
Miss Moreau <darya4@pdx.edu>
2023-03-11 14:24:58 -0800

    Commiting formatting changes

--- src/previous_work.js
+++ src/previous_work.js
@@ -5,19 +5,41 @@ main.style.margin = "0 auto";
 
 const description = document.createElement("p");
 description.classList.add("description");
-description.innerHTML = `Previous Work<br><br>
-<b>October 2021 – March 2022 , October 2022 - Current</b><br>
-Field Services Technician, Portland State University Office Of Information Technology<br>
-My main responsibility as a technician is to log and repair computers. I also provided on site assistance to those whose issue did not warrant having their computer checked in. One of my rarer responsibilities was to set up equipment such as microphones and projectors for Portland State University events. One example of my responsibilities is if a client wanted to turn an old desktop into a shared used desktop, they would send it in to the office and we would purge all information from the device so that it was like new and then we would return the device to the client.
-<br><br>
-<b>March 2022 – September 2022</b><br>
-Intern X, Cambia Health Solutions<br>
-As an intern, I was tasked with assisting other developers on their assignments as well as working on improving the percentage of code that was automatically tested when the systems were run. I helped design a website for employees within Cambia to be able to request information from the team I worked with and I helped train others on how to upkeep that website.
-<br><br>
-<b>April 2023 – September 2023 (Future)</b><br>
-Intern, Portland General Electric<br>
-I have yet to start this position, but I am scheduled to start in April. I will most likely be working on smaller projects and assisting other software developers with their work.
+description.innerHTML = `<h2>Previous Work</h2>`;
+
+const Oit = document.createElement("section");
+Oit.innerHTML = `
+<p><b>October 2021 – March 2022 , October 2022 - Current</b></p>
+<p>Field Services Technician, Portland State University Office Of Information Technology</p>
+<p>My main responsibility as a technician is to log and repair computers. I also provided on site assistance to those whose 
+issue did not warrant having their computer checked in. One of my rarer responsibilities was to set up equipment such as microphones 
+and projectors for Portland State University events. One example of my responsibilities is if a client wanted to turn an old desktop 
+into a shared used desktop, they would send it in to the office and we would purge all information from the device so that it was like 
+new and then we would return the device to the client.</p>
 `;
+
+const Cambia = document.createElement("section");
+Cambia.innerHTML = `
+<p><b>March 2022 – September 2022</b></p>
+<p>Intern X, Cambia Health Solutions</p>
+<p>As an intern, I was tasked with assisting other developers on their assignments as well as working on improving the percentage of 
+code that was automatically tested when the systems were run. I helped design a website for employees within Cambia to be able to request 
+information from the team I worked with and I helped train others on how to upkeep that website.
+</p>
+`;
+
+
+const Pge = document.createElement("section");
+Pge.innerHTML = `
+<p><b>April 2023 – September 2023 (Future)</b></p>
+<p>Intern, Portland General Electric</p>
+<p>I have yet to start this position, but I am scheduled to start in April. I will most likely be working on smaller projects and assisting 
+other software developers with their work.</p>
+`;
+
+const breakLine1 = document.createElement("br");
+const breakLine2 = document.createElement("br");
+const breakLine3 = document.createElement("br");
 
 const style = document.createElement("style");
 style.textContent =
@@ -25,4 +47,10 @@ style.textContent =
 document.head.appendChild(style);
 
 main.appendChild(description);
+main.appendChild(breakLine1);
+main.appendChild(Oit);
+main.appendChild(breakLine2);
+main.appendChild(Cambia);
+main.appendChild(breakLine3);
+main.appendChild(Pge);
 document.body.appendChild(main);
---
+++ src/projects.js
@@ -0,0 +1,35 @@
+const main = document.createElement("main");
+main.classList.add("content");
+main.style.maxWidth = "600px";
+main.style.margin = "0 auto";
+
+const description = document.createElement("p");
+description.classList.add("description");
+description.innerHTML = `<h1>Projects</h1>`;
+
+const shakespearize = document.createElement("section");
+shakespearize.innerHTML = `
+<h2>Shakespearize</h2>
+<p>This project was designed as a final project for a class called "Natural Language Processing". The main purpose of this project was to investigate how to translate text and how to generate random text using the Shakespearean corpus. The random text generator will generate a fake Shakespearean play using a prompted name from the user. In a similar fashion, the Shakespearean translator will translate the given English text into Shakespearean English. The runtime for the random text generator is about seven minutes, and about a minute for the translator.</p>
+<a href="https://github.com/MissMoreau/shakespearize">https://github.com/MissMoreau/shakespearize</a>
+`;
+
+const textBasedGame = document.createElement("section");
+textBasedGame.innerHTML = `
+<br><h2>Text Adventure</h2>
+<p>This project was not originally designed by me, but I provided updates to the code and added multi-language functionality. The goal of this code is to provide a short text-based adventure game. My goal with this project was to modify the code to make it easier to read, change how the text was shown, and add in multi-language capabilities. The game now supports English, Indonesian, Japanese, Klingon, and Quenya with the ability to add more languages.</p>
+<a href="https://github.com/MissMoreau/TextAdventure">https://github.com/MissMoreau/TextAdventure</a>
+`;
+
+const roboSnake = document.createElement("section");
+roboSnake.innerHTML = `
+<br><h2>RoboSnake</h2>
+<p>This project was also not originally designed by me, but I provided updates to the code and added unique agent functionality. The goal of this code is to give a robosnake game where the user will add in new agents for players that the AI will then run. My goal with this project was to modify the code to make it easier to read, refactor the players so that you can add more players without breaking the game, and adding documentation for user's to read and understand how they can add in their players. The game currently supports four different types of players, with the ability to add more players by a user.</p>
+<a href="https://github.com/MissMoreau/RoboSnake">https://github.com/MissMoreau/RoboSnake</a>
+<br><br>`;
+
+main.appendChild(description);
+main.appendChild(shakespearize);
+main.appendChild(textBasedGame);
+main.appendChild(roboSnake);
+document.body.appendChild(main);
