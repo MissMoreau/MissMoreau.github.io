4fad5a1cf1421e6c2cd38ffd0870897247977e74
Miss Moreau <darya4@pdx.edu>
2023-03-11 13:51:16 -0800

    commiting current form changes

---
+++ src/form.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Check if form is empty
+  const formInputs = form.querySelectorAll(".input");
+  const radioInputs = form.querySelectorAll(".radioInput");
+
+  let formEmpty = true;
+  formInputs.forEach((input) => {
+    console.log(input.value, input.tagName);
+    if (input.value !== "") {
+      formEmpty = false;
+    }
+  });
+
+  radioInputs.forEach((input) => {
+    console.log(input.checked, input.tagName);
+    if (input.checked !== false) {
+      formEmpty = false;
+    }
+  });
+
+  if (formEmpty) {
+    console.error("You must enter some data to submit this form");
+    return;
+  }
+
+  // Log form data to console
+  else {
+    const formData = new FormData(form);
+    console.log(
+      "======== Form Submission ========",
+      "\Full Name: " + document.querySelector("#fullName").value,
+      "\nEmail: " + document.querySelector("#email").value,
+      "\nMessage: " + document.querySelector("#message").value
+    );
+  }
+
+  // Clear form data
+  formInputs.forEach((input) => {
+    input.value = "";
+  });
+});
